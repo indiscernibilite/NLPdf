5c71c3f2848ea94130e4db813740e808da356f49
indiscernibilite <105583813+indiscernibilite@users.noreply.github.com>
2023-05-02 21:36:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,75 @@ st.subheader("📊 데이터프레임")
 iris = datasets.load_iris()
 df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 st.dataframe(df)
+
+import openai
+import sys
+from IPython.display import display, HTML
+
+openai.api_key = "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"
+
+# 격리된 local variables
+mylocals = {}
+def myexec(code):
+  global mylocals
+  # print("-- Executing:")
+  print("# code: \n" + code)
+  exec(code, globals(), mylocals)
+def myset(x, v):
+  global mylocals
+  mylocals[x] = v
+def myget(x):
+  return mylocals[x]
+
+#chat GPT와 주고받는 대화를 기록하기 위한 함수. str을 request로 보냄. return도 받아옴.
+def myrequest(req):
+  mymessages.append({"role": "user", "content": req})
+  response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=mymessages,
+  temperature=0,
+  )
+  ret = response['choices'][0]['message']['content']
+  mymessages.append({"role": "assistant", "content": ret})
+  return ret
+
+# ChatGPT한테 시키고 받아온 코드 실행 시킴
+def myexeccode(str):
+  global initialprompt
+  str = initialprompt + "# " + str
+  initialprompt = ""  
+  code = myrequest(str)
+  myexec(code)
+  return myget('df')
+
+# 초기화
+def DataGPT(df):
+  global mylocals
+  global mymessages
+  global initialprompt
+  # initialization code
+  initialcode = (f"import pandas as pd \ndf = pd.DataFrame({df})")
+  # initialize local variables
+  mylocals = {}
+  myset('df', df)
+  display(df)
+  # initialize message history
+  mymessages = [
+        {"role": "system", "content": "You are a smart python developer."},
+    ]
+  initialprompt = """
+      DO NOT EXPLAIN! just put executable code! no double quotation.
+      Write down the code that will COME AFTER the following Python code.
+      Do not repeat the code I wrote!
+      All task will be done in place.
+      ———————————
+      """ + initialcode
+  while True:
+    msg = input("데이터프레임에 적용할 명령어를 입력하세요: ")
+    if msg == "stop":
+      break
+    display(myexeccode(msg))
+  return myget('df')
+
+DataGPT(df)
+
