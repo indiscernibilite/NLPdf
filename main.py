73ade466f3d3572369f5425c51866d95cf312bba
indiscernibilite <105583813+indiscernibilite@users.noreply.github.com>
2023-05-02 21:34:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 st.title("자연어로 데이터프레임 변환하기")
 st.text("데이터프레임에 적용하고 싶은 것을 자연어로 표현하면 그에 따라 변환해줍니다.\n그만 변환하고 싶으면 'stop'을 작성해주세요.")
 
+st.subheader("📊 데이터프레임")
 iris = datasets.load_iris()
 df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-st.table(df)
+st.dataframe(df)
