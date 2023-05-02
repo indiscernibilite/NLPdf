b88c54fe55bf126bca76011ffb48862272fcb4ee
indiscernibilite <105583813+indiscernibilite@users.noreply.github.com>
2023-05-02 21:25:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-!pip install sklearn
+pip3 install sklearn
 
 st.title("자연어로 데이터프레임 변환하기")
 st.text("데이터프레임에 적용하고 싶은 것을 자연어로 표현하면 그에 따라 변환해줍니다.\n그만 변환하고 싶으면 'stop'을 작성해주세요.")
