1f6c68c95fca79388619021fdf5ce49e241caceb
indiscernibilite <105583813+indiscernibilite@users.noreply.github.com>
2023-05-02 21:24:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
-
+!pip install sklearn
 
 st.title("자연어로 데이터프레임 변환하기")
 st.text("데이터프레임에 적용하고 싶은 것을 자연어로 표현하면 그에 따라 변환해줍니다.\n그만 변환하고 싶으면 'stop'을 작성해주세요.")
 
+from sklearn.datasets import load_iris 
 import pandas as pd
 iris = load_iris()
 df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-st.write(df)
+st.dataframe(df)
